a1a4b72fe04dc53bbac684ed5af9f9f37d5f2e90
a.delcroix <a.delcroix@acti.fr>
2020-03-11 16:40:20 +0100

    Remove public_queryable argument to get post types archive

--- src/Helper/PostTypeHelper.php
+++ src/Helper/PostTypeHelper.php
@@ -1,74 +1,70 @@
 <?php
 
-  namespace CPA\Helper;
+    namespace CPA\Helper;
 
-final class PostTypeHelper
-{
-    public static function getArchivesPostTypes()
+    final class PostTypeHelper
     {
-        $args = array(
-        'publicly_queryable'  => true,
-        '_builtin'            => false
-        );
-        $postTypesObjects = get_post_types($args, 'objects');
-        $postTypes = [];
-        foreach ($postTypesObjects as $postType) {
-            $postTypes[$postType->name] = $postType->label;
-        }
-
-        return $postTypes;
-    }
-
-    public static function getArchivesPostTypesPages()
-    {
-        $args = array(
-        'publicly_queryable'  => true,
-        '_builtin'            => false
-        );
-        $postTypesObjects = get_post_types($args, 'objects');
-        $postTypesPages = [];
-        foreach ($postTypesObjects as $postType) {
-            $value = get_option('cpa_' . $postType->name);
-            if ($value) {
-                $postTypesPages[$value] = $postType->label;
+        public static function getArchivesPostTypes()
+        {
+            $args = array(
+                '_builtin'            => false
+            );
+            $postTypesObjects = get_post_types($args, 'objects');
+            $postTypes = [];
+            foreach ($postTypesObjects as $postType) {
+                $postTypes[$postType->name] = $postType->label;
             }
+
+            return $postTypes;
         }
 
-        return $postTypesPages;
-    }
-
-    public static function getArchivePostTypeById($id)
-    {
-        $args = array(
-        'publicly_queryable'  => true,
-        '_builtin'            => false
-        );
-        $postTypesObjects = get_post_types($args, 'objects');
-        foreach ($postTypesObjects as $postType) {
-            $value = get_option('cpa_' . $postType->name);
-            if ($value == $id) {
-                return $postType->name;
+        public static function getArchivesPostTypesPages()
+        {
+            $args = array(
+                '_builtin'            => false
+            );
+            $postTypesObjects = get_post_types($args, 'objects');
+            $postTypesPages = [];
+            foreach ($postTypesObjects as $postType) {
+                $value = get_option('cpa_' . $postType->name);
+                if ($value) {
+                    $postTypesPages[$value] = $postType->label;
+                }
             }
+
+            return $postTypesPages;
         }
 
-        return null;
-    }
-
-    public static function getActiveCustomPostTypePageArchives()
-    {
-        $args = array(
-        'publicly_queryable'  => true,
-        '_builtin'            => false
-        );
-        $postTypesObjects = get_post_types($args, 'objects');
-        $customPostTypePages = [];
-        foreach ($postTypesObjects as $postType) {
-            $value = get_option('cpa_' . $postType->name);
-            if ($value) {
-                $customPostTypePages[$postType->name] = $value;
+        public static function getArchivePostTypeById($id)
+        {
+            $args = array(
+                '_builtin'            => false
+            );
+            $postTypesObjects = get_post_types($args, 'objects');
+            foreach ($postTypesObjects as $postType) {
+                $value = get_option('cpa_' . $postType->name);
+                if ($value == $id) {
+                    return $postType->name;
+                }
             }
+
+            return null;
         }
 
-        return $customPostTypePages;
+        public static function getActiveCustomPostTypePageArchives()
+        {
+            $args = array(
+                '_builtin'            => false
+            );
+            $postTypesObjects = get_post_types($args, 'objects');
+            $customPostTypePages = [];
+            foreach ($postTypesObjects as $postType) {
+                $value = get_option('cpa_' . $postType->name);
+                if ($value) {
+                    $customPostTypePages[$postType->name] = $value;
+                }
+            }
+
+            return $customPostTypePages;
+        }
     }
-}
