119268969acaa4c98e70428c111559b9e646f44f
a.delcroix <a.delcroix@acti.fr>
2020-09-28 16:20:05 +0200

    Add trailing slash to polylang translations

--- src/Controller/PolylangController.php
+++ src/Controller/PolylangController.php
@@ -1,45 +1,45 @@
 <?php
 
-namespace CPA\Controller;
+    namespace CPA\Controller;
 
-use CPA\Helper\LanguageHelper;
+    use CPA\Helper\LanguageHelper;
 
-final class PolylangController
-{
-    public function __construct()
+    final class PolylangController
     {
-        add_filter('pll_the_language_link', array($this, 'setTranslationUrl'), 10, 2);
-        add_filter('pll_translation_url', array($this, 'setTranslationUrl'), 10, 2);
-    }
-
-    public function setTranslationUrl($url, $slug)
-    {
-        if (is_singular())
+        public function __construct()
         {
-            $postType = get_post_type();
-            $correctArchiveUrl = $this->_getPostTypeArchiveUrl($postType, $slug);
-            if ($correctArchiveUrl) {
-                $url = $correctArchiveUrl . basename($url);
+            add_filter('pll_the_language_link', array($this, 'setTranslationUrl'), 10, 2);
+            add_filter('pll_translation_url', array($this, 'setTranslationUrl'), 10, 2);
+        }
+
+        public function setTranslationUrl($url, $slug)
+        {
+            if (is_singular())
+            {
+                $postType = get_post_type();
+                $correctArchiveUrl = $this->_getPostTypeArchiveUrl($postType, $slug);
+                if ($correctArchiveUrl) {
+                    $url = $correctArchiveUrl . basename($url) . '/';
+                }
             }
+            else if (is_post_type_archive())
+            {
+                $postType = get_post_type();
+                $url = $this->_getPostTypeArchiveUrl($postType, $slug);
+            }
+
+            return $url;
         }
-        else if (is_post_type_archive())
+
+        private function _getPostTypeArchiveUrl($postType, $slug)
         {
-            $postType = get_post_type();
-            $url = $this->_getPostTypeArchiveUrl($postType, $slug);
+            $prefix = LanguageHelper::getPrefixBySlug($slug);
+            $pageId = get_option($prefix . $postType);
+
+            if ($pageId) {
+                return get_permalink($pageId);
+            }
+
+            return null;
         }
-
-        return $url;
     }
-
-    private function _getPostTypeArchiveUrl($postType, $slug)
-    {
-        $prefix = LanguageHelper::getPrefixBySlug($slug);
-        $pageId = get_option($prefix . $postType);
-
-        if ($pageId) {
-            return get_permalink($pageId);
-        }
-
-        return null;
-    }
-}
